eefe024bb351f0e05f65211a6a6ff5bf8e1f2cf8
mgomez7139 <mgomez7139@stu.d214.org>
2016-09-30 13:50:36 -0500

    Update TableViewController (2).swift

--- D214 Pathway/TableViewController (2).swift
+++ D214 Pathway/TableViewController (2).swift
@@ -26,12 +26,12 @@ class TableViewController: UIViewController, UITableViewDelegate, UITableViewDat
         if selectedPathway == "agricultureFoodAndNaturalResourcesClasses"
         {
             self.navigationItem.title = "Agriculture Food and Natural Resources Classes"
-            self.tableArray = ["PLTW Introduction to Engineering Design", "PLTW Civil Engineering and Architecture", "AP Physics", "AP Chemistry", "AP Calculus", "AP Statistics", "College Speech Communication", "Entrepreneurship", "Foods 1-2", "Culinary Arts/ProStart 1-2", "AP Environmental Science", "AP Biology", "Zoology"]
+            self.tableArray = ["PLTW Introduction to Engineering Design", "PLTW Civil Engineering and Architecture", "Entrepreneurship/incubatoredu", "Foods 1-2", "Culinary Arts/ProStart 1-2", "College Speech Communication", "AP Physics", "AP Chemistry", "AP Calculus", "AP Statistics", "AP Environmental Science", "AP Biology", "Zoology"]
         }
         else if selectedPathway == "architectureAndConstructionClasses"
         {
             self.navigationItem.title = "Architecture and Construction Classes"
-            self.tableArray = ["PLTW Engineering Design and Development", "PLTW Civil Engineering and Architecture", "PLTW Principals of Engineering Design", "AP Art History", "Art 1", "AP Calculus", "College Speech Communication", "Entrepreneurship", "Graphic Art Design", "Woodworking 1-2", "Drafting/CAD", "Practical Architectural Construction - PAC", "Introduction to Business"]
+            self.tableArray = ["PLTW Engineering Design and Development", "PLTW Civil Engineering and Architecture", "PLTW Principals of Engineering Design", "Art 1", "Entrepreneurship/incubatoredu", "Graphic Art Design", "Woodworking 1-2", "Drafting/CAD", "Practical Architectural Construction - PAC", "Introduction to Business", "College Speech Communication", "AP Art History", "AP Calculus"]
         }
         else if selectedPathway == "artsAudioVideoTechnologyAndCommunicationsClasses"
         {
@@ -41,46 +41,46 @@ class TableViewController: UIViewController, UITableViewDelegate, UITableViewDat
         else if selectedPathway == "businessClasses"
         {
             self.navigationItem.title = "Business Classes"
-            self.tableArray = ["Introduction to Business", "Entrepreneurship", "Honors College Business", "AP Economics", "Honors College Accounting"]
+            self.tableArray = ["Introduction to Business", "Entrepreneurship/incubatoredu", "Honors College Business", "Honors College Accounting", "AP Economics"]
         }
         else if selectedPathway == "educationAndTrainingClasses"
         {
             self.navigationItem.title = "Education and Training Classes"
-            self.tableArray = ["Human Growth and Child Development", "Practicum in Early Childhood Education 1-4", "College Introduction to Early Childhood Education", "AP Psychology", "College Speech Communication", "Entrepreneurship", "Introduction to Business", "Business Management", "Spanish", "French", "Italian", "College Introduction to Education", "Teacher Internship", "Education Academy", "AP Coursework in Curriculum"]
+            self.tableArray = ["Human Growth and Child Development", "Practicum in Early Childhood Education 1-4", "Teacher Internship", "College Introduction to Early Childhood Education", "Education Academy", "Entrepreneurship/incubatoredu", "Introduction to Business", "College Speech Communication", "College Introduction to Education", "AP Psychology", "Spanish", "French", "Italian", "AP Coursework in Curriculum"]
         }
         else if selectedPathway == "financeClasses"
         {
             self.navigationItem.title = "Finance Classes"
-            self.tableArray = ["Introduction to Business", "Entrepreneurship", "Honors College Accounting", "AP Economics", "AP Calculus", "AP Computer Science", "AP Statistics", "Honors College Business", "College Speech Communication"]
+            self.tableArray = ["Introduction to Business", "Entrepreneurship/incubatoredu", "Honors College Accounting", "Honors College Business", "College Speech Communication", "AP Economics", "AP Calculus", "AP Computer Science", "AP Statistics"]
         }
         else if selectedPathway == "governmentAndPublicAdministrationClasses"
         {
             self.navigationItem.title = "Government and Public Administration Classes"
-            self.tableArray = ["Introduction to Business", "AP Psychology", "AP Government", "Honors College Accounting", "College Speech Communication", "Naval Science (NJROTC)", "Spanish", "French", "Italian", "AP European History"]
+            self.tableArray = ["Introduction to Business", "College Speech Communication", "Naval Science (NJROTC)", "Spanish", "French", "Italian", "Honors College Accounting", "AP European History", "AP Psychology", "AP Government",]
         }
         else if selectedPathway == "healthClasses"
         {
             self.navigationItem.title = "Health Classes"
-            self.tableArray = ["Introduction to Healthcare Field", "Medical Terminology", "Nursing Assistant Training", "Medical Science Academy", "AP Biology", "AP Chemistry", "AP Psychology", "College Speech Communication", "Health Occupations Career Observation (Harper)", "Human Physiology"]
+            self.tableArray = ["Introduction to Healthcare Field", "Medical Terminology", "Nursing Assistant Training", "Medical Science Academy", "College Speech Communication", "Health Occupations Career Observation (Harper)", "Human Physiology", "AP Biology", "AP Chemistry", "AP Psychology"]
         }
         else if selectedPathway == "restaurantAndFoodBeveragesClasses"
         {
             self.navigationItem.title = "Restaurant and Food Beverages Classes"
-            self.tableArray = ["Foods 1-2", "Culinary Arts/ProStart 1-2", "District Internship", "College Speech Communication", "Entrepreneurship", "Introduction to Business"]
+            self.tableArray = ["Foods 1-2", "Culinary Arts/ProStart 1-2", "Entrepreneurship/incubatoredu", "Introduction to Business", "District Internship", "College Speech Communication"]
         }
         else if selectedPathway == "humanServicesClasses"
         {
             self.navigationItem.title = "Human Services Classes"
-            self.tableArray = ["Human Growth and Child Development", "Sociology", "AP Psychology", "Sociology of Service Learning", "College Speech Communication", "Small Business Management", "Cosmetology", "Personal Training Certification", "Advanced Strength and Conditioning", "AP Biology", "Human Physiology"]
+            self.tableArray = ["Human Growth and Child Development", "Personal Training Certification", "Advanced Strength and Conditioning", "Sociology",  "Sociology of Service Learning", "Human Physiology", "College Speech Communication", "Small Business Management", "Cosmetology", "AP Biology", "AP Psychology"]
         }
         else if selectedPathway == "technologyClasses"
         {
             self.navigationItem.title = "Technology Classes"
-            self.tableArray = ["Computer Programming", "Mobile App Development 2/AP Computer Science Principles", "Computer Repair A Certification", "AP Computer Science", "Advanced Concepts in Coding and Development", "CISCO Networking Basics", "CISCO Routers and Routering Basics", "Advanced Network Systems and Cybersecurity", "AP Calculus", "AP Chemistry", "AP Physics", "AP Statistics", "College Speech Communication", "Robotics", "Graphic Art Design"]
+            self.tableArray = ["Computer Programming", "AP Computer Science Principles", "Graphic Art Design", "Computer Repair A Certification", "AP Computer Science", "Advanced Concepts in Coding and Development", "CISCO Networking Basics", "CISCO Routers and Routering Basics", "Advanced Network Systems and Cybersecurity", "College Speech Communication", "Robotics", "AP Calculus", "AP Chemistry", "AP Physics", "AP Statistics"]
         }
         else if selectedPathway == "lawEnforcementServicesCorrectionServicesClasses" {
             self.navigationItem.title = "Law Enforcement Services and Correction Services Classes"
-            self.tableArray = ["Law and Individual", "American Law", "Introduction to Fire Science(Harper College)", "Fire Behavior and Combustion(Harper College)", "AP Psychology", "College Speech Communication", "Naval Science (NJROTC)", "Sociology of Service Learning", "Introduction to Criminal Justice(Harper College)", "Introduction to Corrections(Harper College)", "Sociology", "AP Government"]
+            self.tableArray = ["Law and Individual", "American Law", "Introduction to Fire Science(Harper College)", "Fire Behavior and Combustion(Harper College)", "College Speech Communication", "Naval Science (NJROTC)", "Sociology of Service Learning", "Introduction to Criminal Justice(Harper College)", "Introduction to Corrections(Harper College)", "Sociology", "AP Government", "AP Psychology"]
         }
         else if selectedPathway == "manufacturingClasses"
         {
@@ -90,16 +90,16 @@ class TableViewController: UIViewController, UITableViewDelegate, UITableViewDat
         else if selectedPathway == "marketingClasses"
         {
             self.navigationItem.title = "Marketing Classes"
-            self.tableArray = ["Introduction to Business", "Entrepreneurship", "Honors College Business", "AP Economics", "AP Calculus", "AP Statistics", "Honors College Accounting", "College Speech Communication", "Graphic Art Design", "Sports and Entertainment Marketing"]
+            self.tableArray = ["Introduction to Business", "Entrepreneurship/incubatoredu", "College Speech Communication", "Graphic Art Design", "Sports and Entertainment Marketing", "Honors College Business", "Honors College Accounting", "AP Calculus", "AP Economics", "AP Statistics"]
         }
         else if selectedPathway == "scienceTechnologyEngineeringAndMathematicsClasses"
         {
             self.navigationItem.title = "Science, Technology, Engineering and Mathematics Classes"
-            self.tableArray = ["PLTW Introduction to Engineering Design", "PLTW Civil Engineering and Architecture", "PLTW Aerospace Engineering", "PLTW Computer Integrated Manufacturing", "PLTW Digital Electronics", "PLTW Principles of Engineering", "Advanced Machine Technology", "AP Biology", "AP Chemistry", "AP Environmental Science", "AP Physics", "AP Statistics", "College Speech Communication", "Computer Programming", "Drafting/CAD", "Robotics", "AP Calculus", "Introduction to Nanotechnology", "AP Economics", "AP European History", "AP Psychology", "AP US Government", "AP Physics"]
+            self.tableArray = ["PLTW Introduction to Engineering Design", "PLTW Civil Engineering and Architecture", "PLTW Aerospace Engineering", "PLTW Computer Integrated Manufacturing", "PLTW Digital Electronics", "PLTW Principles of Engineering", "Advanced Machine Technology", "College Speech Communication", "Computer Programming", "Drafting/CAD", "Robotics", "Introduction to Nanotechnology",  "AP Calculus", "AP Biology", "AP Chemistry", "AP Environmental Science", "AP Statistics", "AP Economics", "AP European History", "AP Psychology", "AP US Government", "AP Physics"]
         }
         else if selectedPathway == "transportationDistributionAndLogisticsClasses" {
             self.navigationItem.title = "Transportation, Distribution and Logistics Classes"
-            self.tableArray = ["Automotive Systems", "Diagnostic and Testing", "Vocational Automotive", "Power-On-Site Training", "Advanced Machine Technology", "PLTW Introduction to Engineering Design", "College Speech Communication", "Heating Principles(Harper College)", "Heating and Cooling Controls(Harper College)", "Refrigeration Systems(Harper College)", "PLTW Civil Engineering and Architecture"]
+            self.tableArray = ["Automotive Systems", "Diagnostic and Testing", "Vocational Automotive", "Power-On-Site Training", "Advanced Machine Technology", "PLTW Introduction to Engineering Design", "Heating Principles(Harper College)", "Heating and Cooling Controls(Harper College)", "Refrigeration Systems(Harper College)", "PLTW Civil Engineering and Architecture", "College Speech Communication"]
         }
         self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None
 
